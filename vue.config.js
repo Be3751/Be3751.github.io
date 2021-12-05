e287c9eb3c5289bddd4b4f6dcc84813a269be5bb
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2021-12-05 16:37:10 +0900

    install gh-pages and

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/weather-app/'
+    : '/',
+  chainWebpack: config => {
+    config.module.rules.delete('eslint');
+  }
+}
