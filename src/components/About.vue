9cf94c51f028fbba237a96c68f1782e488d3c977
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-01-10 02:14:14 +0900

    put images into cards.

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,24 +5,33 @@
         v-for="(item, index) in items"
         :key="index"
       >
-        <v-row class="white" style="height: 400px;">
-          <!-- <v-col cols="12" sm="6" md="6" lg="6" xl="6" style="background-color: white">
-            <v-img :src="item.imgSrc" width="200px"></v-img>
-          </v-col> -->
-          <v-col cols="12" sm="6" md="6" lg="12" xl="12" style="background-color: white">
-            <v-card
-            elevation="3"
-            >
-              <v-card-title>
-                {{ item.name }}
-              </v-card-title>
-              <v-card-text>
-                {{ item.text }}
-              </v-card-text>
-              <v-img :src="item.imgSrc" width="200px"></v-img>
-            </v-card>
-          </v-col>
-        </v-row>
+  
+          <!-- <v-row class="white" style="height: 400px;">
+            <v-col cols="12" sm="6" md="6" lg="6" xl="6" style="background-color: white"> -->
+              <v-card
+                elevation="3"
+              >
+                <div class="d-flex flex-no-wrap justify-space-between">
+                  <div>
+                    <v-card-title
+                      class="text-h5"
+                      v-text="item.name"
+                    >
+                    </v-card-title>
+                    <v-card-text
+                      v-text="item.text"
+                    >
+                    </v-card-text>
+                  </div>
+                  <v-img :src="item.imgSrc" width="200px"></v-img>
+                </div>
+              </v-card>
+            <!-- </v-col> -->
+            <!-- <v-col cols="12" sm="6" md="6" lg="6" xl="6" style="background-color: white" > -->
+              
+            <!-- </v-col>
+          </v-row> -->
+
       </v-list-item>
     </v-list>
   </v-container>
