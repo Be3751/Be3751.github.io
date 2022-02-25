7767d03bd09a37fc1785686f4cd93cac962e1e55
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-02-25 23:54:14 +0900

    added card component for showing each product #11

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -1,17 +1,26 @@
 <template>
   <v-container>
-    Works
+    <v-row>
+      <v-col v-for="product, i in this.products" :key="i">
+        <Product></Product>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-  export default {
-    name: 'Works',
-    data: () => ({
-      title: "/Works"
-    }),
-    mounted() {
-      this.$emit("catchTitle", this.title)
-    }
+import Product from "@/components/Product"
+export default {
+  name: 'Works',
+  components: {
+    Product
+  },
+  data: () => ({
+    title: "/Works",
+    products: [1,2,3],
+  }),
+  mounted() {
+    this.$emit("catchTitle", this.title)
   }
+}
 </script>
