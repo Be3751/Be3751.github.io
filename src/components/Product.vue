931adc39b7d22613396760332083e1fd5fb24b02
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-03-03 00:41:18 +0900

    put github info into Works, Product, ProductDetail components

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -15,7 +15,7 @@
 
         <v-card-actions>
         <v-btn color="orange lighten-2" text @click="showDetail">Detail</v-btn>
-        <v-btn icon href="https://github.com/Be3751/mypage" class="mr-10 ml-2">
+        <v-btn icon :href="product.reposUrl" class="mr-10 ml-2">
           <v-img
             class="shrink mb-1"
             contain
@@ -33,7 +33,7 @@
             <div v-show="show">
                 <v-divider></v-divider>
                 <v-card-text>
-                I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+                {{ product.summary }}
                 </v-card-text>
             </div>
         </v-expand-transition>
@@ -53,13 +53,13 @@ export default {
 
   },
   mounted() {
-    console.log(this.product);
+    // console.log(this.product);
   },
   methods: {
       showDetail: function(){
           this.$router.push({
               name: "product-detail",
-              params: { id: 1}
+              params: { id: "prdct_" + this.product.title , product:this.product }
           })
       }
   }
