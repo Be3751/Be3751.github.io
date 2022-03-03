83546744e4f8a37ab3bb777c665248193bf61ed1
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-03-03 23:05:56 +0900

    chenged the background color of cards

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,7 +2,7 @@
   <v-container class="d-flex justify-center" style="height: 1000px; max-width: 2400px;">
     <v-list>
       <v-list-item v-for="(item, index) in items" :key="index">
-        <v-card elevation="3" width="90vw" class="mb-8">  
+        <v-card elevation="3" width="90vw" class="mb-8 " color="#EEEEEE">  
           <v-card-title v-text="item.name" class="text-h4 mt-5 mb-5 d-flex justify-center"></v-card-title>
           <v-row
             style="height: auto;"
