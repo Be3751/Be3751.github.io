e962ed7d23ea47559451edb801ebe88d7f707450
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-03-05 17:52:54 +0900

    made a new design of Home for smartphones

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,8 @@
   <v-container>
     <v-row>
       <v-col cols="12">
-        <v-img src="@/assets/img/home_design.jpg"></v-img>
+        <v-img src="@/assets/img/home_design.jpg" class="hidden-xs-only"></v-img>
+        <v-img src="@/assets/img/home_design_phone.png" class="hidden-sm-and-up"></v-img>
       </v-col>
     </v-row>
   </v-container>
