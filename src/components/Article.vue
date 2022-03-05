ad9df7087ee159db18b8215a5b9fa3a12c009494
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-03-05 18:56:48 +0900

    changed font size and position of title and removed summary

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -8,14 +8,14 @@
       height="200px"
       :src="article.image.url"
     >
-      <v-card-title>{{ article.title }}</v-card-title>
     </v-img>
 
     <v-card-subtitle class="pb-0 pt-2">作成日：{{ article.createdAt }}</v-card-subtitle>
     <v-card-subtitle class="pb-1 pt-1">更新日：{{ article.updatedAt }}</v-card-subtitle>
 
+    <!-- <v-card-title>{{ article.title }}</v-card-title> -->
     <v-card-text class="text--primary pt-2 pb-0">
-      <div>{{ article.summary }}</div>
+      <div>{{ article.title }}</div>
     </v-card-text>
 
     <v-card-actions>
