af023f7d71f423d1af2bb710e0562af4f2cdbadc
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-03-06 22:53:22 +0900

    modified for using custom domain

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/mypage/'
-    : '/',
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/mypage/'
+  //   : '/',
 
+  // 独自ドメイン用
+  publicPath: '/',
+  
   chainWebpack: config => {
     config.module.rules.delete('eslint');
   },
