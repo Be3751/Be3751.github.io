ee572f2714131ba7eed0379d4ce5f29b3d690d1e
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-03-09 01:17:13 +0900

    separated each info into cards

---
+++ src/components/BasicInfo.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-card width="90vw" height="auto" elevation="3" class="mb-8 " color="#EEEEEE">  
+        <v-card-title v-text="items.name" class="text-h4 mt-5 mb-5 d-flex justify-center"></v-card-title>
+        <v-row style="height: auto;" justify="center" align-content="center" class="mb-5">
+        <v-col cols="12" sm="8" md="5" lg="4" xl="3">
+            <v-card-text v-text="items.text1" class="pt-1 pb-1"></v-card-text>
+            <v-card-text v-text="items.text2" class="pt-1 pb-1"></v-card-text>
+            <v-card-text v-text="items.text3" class="pt-1 pb-1"></v-card-text>
+            <v-card-text v-text="items.text4" class="pt-1 pb-1"></v-card-text>
+        </v-col>
+        <v-col cols="12" sm="8" md="5" lg="4" xl="3" class="d-flex justify-center" style="height: 300px">
+            <v-img :src="items.imgSrc" max-width="180" max-height="180" aspect-ratio="4/4" class=""></v-img>
+        </v-col>
+        </v-row>
+    </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'BasicInfo',
+    methods:{
+        
+    },
+    data: () => ({
+      items: {
+        name: "Basic Information",
+        text1: "所属: 同志社大学M1（24卒）",
+        text2: "研究テーマ: 画像が喚起する感情の推定をするモデルの構築",
+        text3: "趣味: 猫, 洋服, 銭湯, スマブラ, 動画鑑賞（主にYouTubeとPrime Video）",
+        text4: "最近の興味: GoによるAPI実装, 応用情報, 英検1級",
+        imgSrc: require('@/assets/img/gura_icon.png')
+      },
+    }),
+    mounted() {
+      
+    }
+  }
+</script>
+<style>
+
+</style>
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,72 +1,34 @@
 <template>
   <v-container class="d-flex justify-center" style="max-width: 2400px;">
-    <v-list>
-      <v-list-item v-for="(item, index) in items" :key="index">
-        <v-card width="90vw" height="auto" elevation="3" class="mb-8 " color="#EEEEEE">  
-          <v-card-title v-text="item.name" class="text-h4 mt-5 mb-5 d-flex justify-center"></v-card-title>
-          <v-row style="height: auto;" justify="center" align-content="center" class="mb-5">
-            <v-col cols="12" sm="8" md="5" lg="4" xl="3">
-              <v-card-text v-text="item.text1" class="pt-1 pb-1"></v-card-text>
-              <v-card-text v-text="item.text2" class="pt-1 pb-1"></v-card-text>
-              <v-card-text v-text="item.text3" class="pt-1 pb-1"></v-card-text>
-              <v-card-text v-text="item.text4" class="pt-1 pb-1"></v-card-text>
-              <v-card-text v-text="item.text5" class="pt-1 pb-1"></v-card-text>
-              <v-card-text v-text="item.text6" class="pt-1 pb-1"></v-card-text>
-              <v-card-text v-text="item.text7" class="pt-1 pb-1"></v-card-text>
-            </v-col>
-            <v-col cols="12" sm="8" md="5" lg="4" xl="3" class="d-flex justify-center" style="height: 300px">
-              <v-img :src="item.imgSrc" max-width="180" max-height="180" aspect-ratio="4/4" class=""></v-img>
-            </v-col>
-          </v-row>
-        </v-card>
-      </v-list-item>
-    </v-list>
+    <v-row>
+      <v-col class="d-flex justify-center"><BasicInfo></BasicInfo></v-col>
+      <v-col class="d-flex justify-center"><Skill></Skill></v-col>
+      <v-col class="d-flex justify-center"><Experience></Experience></v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-  export default {
-    name: 'Profile',
-    methods:{
-        
-    },
-    data: () => ({
-      title: "/Profile",
-      items: {
-        item1: {
-          name: "Basic Information",
-          text1: "所属: 同志社大学M1（24卒）",
-          text2: "研究テーマ: 画像が喚起する感情の推定をするモデルの構築",
-          text3: "趣味: 猫, 洋服, 銭湯, スマブラ, 動画鑑賞（主にYouTubeとPrime Video）",
-          text4: "最近の興味: GoによるAPI実装, 応用情報, 英検1級",
-          imgSrc: require('@/assets/img/gura_icon.png')
-        },
-        item2: {
-          name: "Skill",
-          text1: "言語: Python(often), JavaScript(sometimes), C#(sometimse), Java(sometimes), Go(learning)",
-          text2: "フレームワーク: Django, Flask, WPF",
-          text3: "インフラ: AWS(EC2, S3, Lambda, DynamoDB, etc...), Docker",
-          text4: "その他: Git/GitHub, Linux",
-          text5: "語学: TOEIC855, 英検準1級",
-          imgSrc: require('@/assets/img/vue_logo.png')
-        },
-        item3: {
-          name: "Experience",
-          text1: "大学の講義やPyConJP主催のセミナーを通じてプログラミング学習の開始/学部1年, 2018年",
-          text2: "弊学文化祭アプリの開発/学部2年, 2019年",
-          text3: "ケンブリッジ大学短期留学プログラムへの参加/学部2年, 2019年",
-          text4: "英語学習に注力, オンライン型英語レッスン事業の開始/学部3年, 2020年",
-          text5: "ジャスチャー判定を用いた教育アプリの開発/学部4年, 2021年",
-          text6: "Webアプリ自動テストツールの開発/学部4年, 2021年",
-          text7: "画像が喚起する感情を推定する深層学習モデルの構築/学部4年, 2021年",
-          imgSrc: require('@/assets/img/pc_logo.png')
-        }
-      },
-    }),
-    mounted() {
-      this.$emit("catchTitle", this.title)
-    }
+import BasicInfo from "@/components/BasicInfo"
+import Skill from "@/components/Skill"
+import Experience from "@/components/Experience"
+export default {
+  name: 'Profile',
+  components: {
+    BasicInfo,
+    Skill,
+    Experience
+  },
+  methods:{
+      
+  },
+  data: () => ({
+    title: "/Profile",
+  }),
+  mounted() {
+    this.$emit("catchTitle", this.title)
   }
+}
 </script>
 <style>
 
