13f8f7293ab1a8cf50562c7488e9801632b06dde
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-03-10 01:59:19 +0900

    added icons refering to each social media

--- src/components/BasicInfo.vue
+++ src/components/BasicInfo.vue
@@ -1,16 +1,26 @@
 <template>
     <v-card width="80vw" height="auto" elevation="3" class="mb-8 mt-8" color="#EEEEEE">  
-        <v-card-title v-text="items.name" class="text-h4 mt-5 mb-5 d-flex justify-center"></v-card-title>
-        <v-row style="height: auto;" justify="center" align-content="center" class="mb-5">
-        <v-col cols="12" sm="8" md="5" lg="4" xl="3">
-            <v-card-text v-text="items.text1" class="pt-1 pb-1"></v-card-text>
-            <v-card-text v-text="items.text2" class="pt-1 pb-1"></v-card-text>
-            <v-card-text v-text="items.text3" class="pt-1 pb-1"></v-card-text>
-            <v-card-text v-text="items.text4" class="pt-1 pb-1"></v-card-text>
-        </v-col>
-        <v-col cols="12" sm="8" md="5" lg="4" xl="3" class="d-flex justify-center" style="height: 300px">
+        <v-row class="mt-1" style="height:auto;" justify="center" align-content="center">
+          <v-card-title class="text-h4 mt-5 d-flex justify-center">Basic Information</v-card-title>
+        </v-row>
+        <v-row style="height:auto;" justify="center" align-content="center">
+          <v-col cols="12" sm="8" md="5" lg="4" xl="3">
+              <v-card-text v-text="items.text1" class="pt-1 pb-1"></v-card-text>
+              <v-card-text v-text="items.text2" class="pt-1 pb-1"></v-card-text>
+              <v-card-text v-text="items.text3" class="pt-1 pb-1"></v-card-text>
+              <v-card-text v-text="items.text4" class="pt-1 pb-1"></v-card-text>
+          </v-col>
+          <v-col cols="12" sm="8" md="5" lg="4" xl="3" class="d-flex justify-center" style="height:200px">
             <v-img :src="items.imgSrc" max-width="180" max-height="180" aspect-ratio="4/4" class=""></v-img>
-        </v-col>
+          </v-col>
+        </v-row>
+        <v-row style="margin-left: 20%;" class="mb-7 mt-5">
+            <v-btn href="https://github.com/Be3751" icon color="black" class="ml-5">
+              <v-icon large>mdi-github</v-icon>
+            </v-btn>
+            <v-btn href="https://twitter.com/Blossomrail" icon color="blue" class="ml-5">
+              <v-icon large>mdi-twitter</v-icon>
+            </v-btn>
         </v-row>
     </v-card>
 </template>
@@ -23,7 +33,6 @@
     },
     data: () => ({
       items: {
-        name: "Basic Information",
         text1: "所属: 同志社大学M1（24卒）",
         text2: "研究テーマ: 画像が喚起する感情の推定をするモデルの構築",
         text3: "趣味: 猫, 洋服, 銭湯, スマブラ, 動画鑑賞（主にYouTubeとPrime Video）",
