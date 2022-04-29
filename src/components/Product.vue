1bdc36e7de0fc91ac30ab1e3218202c056db9e7e
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-04-29 10:50:45 +0900

    fixed id for url

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -58,7 +58,7 @@ export default {
       showDetail: function(){
           this.$router.push({
               name: "product-detail",
-              params: { id: "prd_" + this.product.title , product:this.product }
+              params: { id: this.product.id,  product:this.product }
           })
       }
   }
